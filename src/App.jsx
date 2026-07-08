ab4929fd608f2ddf29ba1de02b61aa7d93363c94
Vera Krumpan <filimonovavera96@gmail.com>
2026-07-08 14:50:47 +0300

    feat: add main structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className="min-h-screen w-full">
+    </div>
+  );
 }
 
-export default App
+export default App;
